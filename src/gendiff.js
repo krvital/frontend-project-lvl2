a615db3905dba6569c96dfc1655fdf09d0e3d86f
Vitalii Kravtsov <mail@krvital.pro>
2020-10-04 21:49:51 +0600

    add tests and linters

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,52 +1,57 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
 
-function calcDiff(data1, data2) {
-  const result = [];
-  const merged = { ...data1, ...data2 };
+export function calcDiff(data1, data2) {
+  const allKeys = Object.keys({ ...data1, ...data2 });
 
-  for (let key of Object.keys(merged)) {
+  return allKeys.reduce((acc, key) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
-    if (value1 !== undefined && value2 !== undefined) {
-      if (value1 === value2) {
-        result.push({ key, value: value1, status: "unmodified" });
-      } else {
-        result.push({ key, value: value1, status: "deleted" });
-        result.push({ key, value: value2, status: "added" });
-      }
-    } else {
-      if (value2 === undefined) {
-        result.push({ key, value: value1, status: "deleted" });
-      } else {
-        result.push({ key, value: value2, status: "added" });
-      }
-    }
-  }
+    const isKeyExistsInBoth = _.has(data1, key) && _.has(data2, key);
+    const areValuesEqual = value1 === value2;
 
-  return result;
+    if (isKeyExistsInBoth && areValuesEqual) {
+      return [...acc, { key, value: value1, status: 'unmodified' }];
+    }
+
+    if (isKeyExistsInBoth && !areValuesEqual) {
+      return [
+        ...acc,
+        { key, value: value1, status: 'deleted' },
+        { key, value: value2, status: 'added' },
+      ];
+    }
+
+    return [
+      ...acc,
+      _.has(data1, key)
+        ? { key, value: value1, status: 'deleted' }
+        : { key, value: value2, status: 'added' },
+    ];
+  }, []);
 }
 
 function formatDiff(diff) {
   const statusToSymbol = {
-    unmodified: " ",
-    added: "+",
-    deleted: "-",
+    unmodified: ' ',
+    added: '+',
+    deleted: '-',
   };
 
   const lines = diff
     .map(
       (line) => `  ${statusToSymbol[line.status]} ${line.key}: ${line.value}`
     )
-    .join("\n");
+    .join('\n');
 
   return `{\n${lines}\n}`;
 }
 
 export default function genDiff(filepath1, filepath2) {
-  const file1 = fs.readFileSync(path.resolve(process.cwd(), filepath1), "utf8");
-  const file2 = fs.readFileSync(path.resolve(process.cwd(), filepath2), "utf8");
+  const file1 = fs.readFileSync(path.resolve(process.cwd(), filepath1), 'utf8');
+  const file2 = fs.readFileSync(path.resolve(process.cwd(), filepath2), 'utf8');
 
   const data1 = JSON.parse(file1);
   const data2 = JSON.parse(file2);
