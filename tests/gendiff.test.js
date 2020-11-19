a46939d07a2b2b8d04d79e6d5eb32a18472dea4f
Vitalii Kravtsov <mail@krvital.pro>
2020-11-19 20:30:03 +0600

    add test for yml files

--- tests/gendiff.test.js
+++ tests/gendiff.test.js
@@ -1,9 +1,21 @@
+import path, { dirname } from 'path';
 import { test, expect } from '@jest/globals';
-import { calcDiff } from '../src/gendiff';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import genDiff, { calcDiff } from '../src/gendiff';
+
+const __filename = fileURLToPath(import.meta.url); // eslint-disable-line no-underscore-dangle
+const __dirname = dirname(__filename); // eslint-disable-line no-underscore-dangle
+const getFixturePath = (filename) => path.join(__dirname, '.', '__fixtures__', filename);
 
 const o1 = { a: 1 };
 const o2 = { b: 2 };
 const o4 = { a: 5, b: 2 };
+
+let diff;
+beforeAll(() => {
+  diff = readFileSync(getFixturePath('diff.txt'), 'utf-8');
+});
 
 test('diff for equal objects', () => {
   expect(calcDiff(o1, o1)).toEqual([
@@ -28,4 +40,18 @@ test('diff where value of same key has been modified', () => {
 
 test('diff value for empty objects', () => {
   expect(calcDiff({}, {})).toEqual([]);
+});
+
+test('diff of two json files', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+
+  expect(genDiff(filepath1, filepath2)).toMatch(diff);
+});
+
+test('diff of two yaml files', () => {
+  const filepath1 = getFixturePath('file1.yml');
+  const filepath2 = getFixturePath('file2.yml');
+
+  expect(genDiff(filepath1, filepath2)).toMatch(diff);
 });
