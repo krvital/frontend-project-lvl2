79467eb729c514cc840721d7b69ef3630cf4442b
Vitalii Kravtsov <mail@krvital.pro>
2021-03-28 10:34:09 +0600

    update eslint settings; fix linter errors

--- tests/gendiff.test.js
+++ tests/gendiff.test.js
@@ -4,19 +4,13 @@ import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
 import genDiff from '../src/gendiff';
 
-const __filename = fileURLToPath(import.meta.url); // eslint-disable-line no-underscore-dangle
-const __dirname = dirname(__filename); // eslint-disable-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '.', '__fixtures__', filename);
 
-let diff;
-let plainDiff;
-let jsonDiff;
-
-beforeAll(() => {
-  diff = readFileSync(getFixturePath('diff.txt'), 'utf-8');
-  plainDiff = readFileSync(getFixturePath('diff-plain.txt'), 'utf-8');
-  jsonDiff = readFileSync(getFixturePath('diff-json.txt'), 'utf-8');
-});
+const diff = readFileSync(getFixturePath('diff.txt'), 'utf-8');
+const plainDiff = readFileSync(getFixturePath('diff-plain.txt'), 'utf-8');
+const jsonDiff = readFileSync(getFixturePath('diff-json.txt'), 'utf-8');
 
 test('tree diff of two nested files', () => {
   const filepath1 = getFixturePath('file1.yml');
