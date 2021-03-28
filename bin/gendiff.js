bbd2e77441fec3b418763bc0075eac241f80691d
Vitalii Kravtsov <mail@krvital.pro>
2021-03-28 10:25:06 +0600

    move run logic to bin file; add index file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
+import commander from 'commander';
+import genDiff from '../index.js';
 
-import run from '../src/cli.js';
+const program = new commander.Command();
 
-run();
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'tree')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, params) => {
+    const diff = genDiff(filepath1, filepath2, params.format);
+    console.log(diff);
+  })
+  .parse(process.argv);
